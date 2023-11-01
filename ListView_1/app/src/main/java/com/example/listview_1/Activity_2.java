ae6ef900bed7b07a664ab154e2aab1202bb4cc8f
Chekesh <122931647+Chekesh@users.noreply.github.com>
2023-11-01 12:30:13 +0300

    1.0

--- ListView_1/app/src/main/java/com/example/listview_1/Activity_2.java
+++ ListView_1/app/src/main/java/com/example/listview_1/Activity_2.java
@@ -1,47 +1,46 @@
 package com.example.listview_1;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class Activity_2 extends AppCompatActivity {
 
-    int i;
-    EditText editTextTop;
-    EditText editText;
+    int i;  // Индекс элемента списка
+    EditText editTextTop;  // Поле для ввода темы
+    EditText editText;     // Поле для ввода текста
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
+        // Нахождение полей ввода в макете Activity_2
         editTextTop = findViewById(R.id.editTextTopic);
         editText = findViewById(R.id.editText);
 
+        // Получение данных из предыдущей активности
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            i = extras.getInt("object");
-            editTextTop.setText(MainActivity.list[i]);
-            editText.setText(MainActivity.list_znach[i]);
+            i = extras.getInt("object");  // Получение индекса выбранного элемента
+            editTextTop.setText(MainActivity.list[i]);  // Установка текста в поле для темы
+            editText.setText(MainActivity.list_znach[i]);  // Установка текста в поле для текста
         }
     }
 
+    // Метод, вызываемый при нажатии кнопки "Добавить результат"
     public void onClickBtnAddResult(View view) {
         Intent data = new Intent();
-        data.putExtra("Topic", String.valueOf(editTextTop.getText()));
-        data.putExtra("Text", String.valueOf(editText.getText()));
-        data.putExtra("id", String.valueOf(i));
+
+        // Помещение данных в Intent для возврата обратно в предыдущую активность
+        data.putExtra("Topic", String.valueOf(editTextTop.getText()));  // Добавление темы
+        data.putExtra("Text", String.valueOf(editText.getText()));       // Добавление текста
+        data.putExtra("id", String.valueOf(i));                         // Добавление индекса
+
+        // Установка результата как "RESULT_OK" и передача данных обратно
         setResult(RESULT_OK, data);
-        finish();
+        finish();  // Завершение текущей активности и возврат к предыдущей
     }
 }
--- ListView_1/app/src/main/java/com/example/listview_1/MainActivity.java
+++ ListView_1/app/src/main/java/com/example/listview_1/MainActivity.java
@@ -17,10 +17,17 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Массив элементов для списка
     static String[] list = {"В магазин", "Дела", "Работа", "Не забыть", "Поговорить"};
-    static int in = 1;
 
-    static String[] list_znach = {"Cходить в магазин купить:\n 1) Молоко \n 2) Макакронны \n 3) Сахар \n 4) Чай \n", "Дела", "Работа", "ЗАбыть", "не поговорить"};
+    // Массив для хранения дополнительной информации о элементах списка
+    static String[] list_znach = {
+            "Cходить в магазин купить:\n 1) Молоко \n 2) Макакронны \n 3) Сахар \n 4) Чай \n",
+            "Дела",
+            "Работа",
+            "ЗАбыть",
+            "не поговорить"
+    };
 
     ArrayAdapter<String> adapter;
 
@@ -29,39 +36,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Нахождение ListView в макете
         ListView ListView = findViewById(R.id.list);
 
+        // Создание ArrayAdapter для ListView
         adapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 list
         );
+        // Установка адаптера для ListView
         ListView.setAdapter(adapter);
 
+        // Установка слушателя нажатия на элемент списка
         ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // Создание намерения для запуска Activity_2 и передача данных в него
                 Intent intent = new Intent(MainActivity.this, Activity_2.class);
                 intent.putExtra("object", i);
-                mStartForResult.launch(intent);
+                mStartForResult.launch(intent); // Запуск Activity_2 и ожидание результата
             }
         });
     }
 
+    // Регистрация для получения результата от Activity_2
     ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
-
+                    // Обработка результата от Activity_2
                     if (result.getResultCode() == Activity.RESULT_OK) {
+                        // Извлечение данных из возвращённого намерения
                         Intent intent = result.getData();
                         String Topic = intent.getStringExtra("Topic");
                         String Text = intent.getStringExtra("Text");
                         String i = intent.getStringExtra("id");
+
+                        // Обновление массивов новыми данными
                         list[Integer.parseInt(i)] = String.valueOf(Topic);
                         list_znach[Integer.parseInt(i)] = String.valueOf(Text);
+
+                        // Уведомление адаптера о изменении данных
                         adapter.notifyDataSetChanged();
                     } else {
+                        // Показ сообщения toast в случае ошибки
                         Toast.makeText(MainActivity.this, "Ошибка", Toast.LENGTH_LONG).show();
                     }
                 }
